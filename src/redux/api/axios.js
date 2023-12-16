cd66f15c73c1bcbd3c283507fc5d45fb9bb2d017
bassm3462 <bassmhu786@gmail.com>
2023-12-16 10:06:02 +0300

    add active account

--- src/redux/api/axios.js
+++ src/redux/api/axios.js
@@ -1,6 +1,6 @@
-// export const backendURL = 'https://test-ecommerce-server.onrender.com';
+export const backendURL = 'https://test-ecommerce-server.onrender.com';
 
-export const backendURL = 'http://127.0.0.1:4000';
+// export const backendURL = 'http://127.0.0.1:4000';
 
 export const token = localStorage.getItem("token")
 export const info = JSON.parse(localStorage.getItem("user"))
